011496cef828daf37d003bc9eaa8b8613a5a0c74
GeezNuts <georgesuitela05@gmail.com>
2025-07-12 19:41:13 +0700

    minigame1

--- My project (1)/Assets/Script/MiniGame1/TimerManager.cs
+++ My project (1)/Assets/Script/MiniGame1/TimerManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float timeLimit = 15f;
+    private float timer;
+
+    public TMP_Text timerText;
     void Start()
     {
-        
+        timer = timeLimit;
+        Time.timeScale = 1f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        timerText.text = Mathf.CeilToInt(timer).ToString();
+
+        if (timer <= 0)
+            EndGame(false);
+    }
+
+    void EndGame(bool win)
+    {
+        timerText.text = win ? "You Win!" : "Time's Up!";
+        Time.timeScale = 0f;
     }
 }
